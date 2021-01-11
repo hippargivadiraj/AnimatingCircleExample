b75c6a9a970fad616a584a1ee23bf49e76e267e3
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-01-10 16:16:17 -0800

    Initial Commit

---
+++ AnimatingCircleExample/AnimatingCircleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatingCircleExampleApp.swift
+//  AnimatingCircleExample
+//
+//  Created by Vadiraj Hippargi on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatingCircleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
